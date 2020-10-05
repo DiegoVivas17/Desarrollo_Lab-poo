89266d6e9610c32e093316d368275e2eecd173e8
DiegoVivas13 <62918291+DiegoVivas13@users.noreply.github.com>
2020-10-04 22:07:23 -0500

    Punto 2

---
+++ class list/Dealer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace class_list
+{
+    public class Dealer
+    {
+        private List<Card> deck;
+
+        public void Generate()
+        {
+            deck = new List<Card>();
+
+            char[] suits = { '♥', '♦', '♣', '♠' };
+
+            string[] symbols = { "A","2","3","4","5","6",
+                                 "7","8","9","10","J","Q","K",};
+
+            foreach (char su in suits)
+            {
+                foreach (string sy in symbols)
+                {
+                    Card c = new Card(su, sy);
+                    deck.Add(c);
+                }
+            }
+        }
+
+    }
+}
